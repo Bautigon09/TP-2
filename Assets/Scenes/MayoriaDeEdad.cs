286b01381140a3e4894beb458c7de4f03865fbc2
Bautigon09 <102154628+Bautigon09@users.noreply.github.com>
2022-04-13 15:37:03 -0300

    Initial commit

---
+++ Assets/Scenes/MayoriaDeEdad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edad = 0;
+    public Text miTexto;
+    
+    void Start()
+    {
+        if (edad >= 18)
+    {
+            miTexto.text = "Es mayor de edad";
+        Debug.Log("Es mayor de edad");
+
+    }
+        else
+    {
+            miTexto.text = "No es mayor de edad";
+            Debug.Log("Es menor de edad");
+    }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
